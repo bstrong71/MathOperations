43e91b86e405672a3bea98f8926aed40c2bb932f
Bernard Strong <bstrong71@gmail.com>
2017-09-11 15:36:47 -0400

    added functionality for addition, subtraction, division, multiplication, and remainder

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,22 @@ public class Main {
         System.out.println("Enter the second number: ");
         scanner = new Scanner(System.in);
         String secondNumber = scanner.nextLine();
-        double firstDouble = Double.parseDouble(firstNumber); // convert inputs to doubles
-        double secondDouble = Double.parseDouble(secondNumber);
+        double operand1 = Double.parseDouble(firstNumber); // convert inputs to doubles
+        double operand2 = Double.parseDouble(secondNumber);
+        double sum = operand1 + operand2;
+        double difference = operand1 - operand2;
+        double division = operand1 / operand2;
+        double multiplication = operand1 * operand2;
+        double remainder = operand1 % operand2;
 
+        showResults(sum, difference, division, multiplication, remainder);
+
+    }
+    public static void showResults(double sum, double difference, double division, double multiplication, double remainder) {
+        System.out.println("The sum is " + sum + ".");
+        System.out.println("The difference is " + difference + ".");
+        System.out.println("The quotient is " + division + ".");
+        System.out.println("The product is " + multiplication + ".");
+        System.out.println("The remainder is " + remainder + ".");
     }
 }
